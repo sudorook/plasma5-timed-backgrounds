1f0aa78b2c0a5033dae0ee6b7fb21136e2e5c0f5
sudorook <daemon@nullcodon.com>
2023-02-06 16:19:46 -0600

    add manifest templates and script for propagating updates

---
+++ templates/create_manifest.sh
@@ -0,0 +1,82 @@
+#! /bin/bash
+set -eu
+
+ROOT="$(dirname "${0}")"
+
+# Functions
+
+function make_manifest {
+  local file="${1}"
+  local suffix
+  local name="${NAME}"
+  if [[ "${name}" =~ \-4k ]]; then
+    # name="$(echo "${name}" | sed -e "s/-4k//g")"
+    name="${name//-4k/}"
+    suffix=4k
+  fi
+  sed -e "s/\(\"FileName\":\) \"\(.*\)\"/\1 \"${name}-\2${suffix:+-${suffix}}\.${IMG}\"/g" \
+    "${file}" > "${DIR}/${NAME}.json"
+}
+
+OPTIONS=d:n:i:t:
+LONGOPTIONS=dir:,name:,image:,type:
+PARSED=$(getopt -o ${OPTIONS} --long ${LONGOPTIONS} -n "${0}" -- "${@}")
+eval set -- "${PARSED}"
+
+while [ ${#} -ge 1 ]; do
+  case "${1}" in
+    -d|--dir)
+      DIR="${2}"
+      shift 2
+      ;;
+    -n|--name)
+      NAME="${2}"
+      shift 2
+      ;;
+    -i|--image)
+      IMG="${2}"
+      shift 2
+      ;;
+    -t|--type)
+      TYPE="${2}"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      show_error "ERROR"
+      exit 3
+      ;;
+  esac
+done
+
+
+# Defaults and checks
+NAME="${NAME:-$(basename "${DIR}")}"
+IMG="${IMG:-jpg}"
+! [ -d "${DIR}" ] && exit 3
+[ -z "${TYPE}" ] && exit 3
+
+case "${TYPE,,}" in
+  day-night|daynight)
+    make_manifest "${ROOT}"/day-night.json
+    ;;
+  day-sunset-night|daysunsetnight)
+    make_manifest "${ROOT}"/day-sunset-night.json
+    ;;
+  morning-day-sunset-night|morningddaysunsetnight)
+    make_manifest "${ROOT}"/morning-day-sunset-night.json
+    ;;
+  morning-day-sunset-night-static|morningdaysunsetnightstatic|morning-day-sunset-night_static|morning-day-sunset-night-fixed|morningdaysunsetnightfixed|morning-day-sunset-night_fixed)
+    make_manifest "${ROOT}"/morning-day-sunset-night_fixed.json
+    ;;
+  morning-day-evening-night-static|morningdayeveningnightstatic|morning-day-evening-night_static|morning-day-evening-night-fixed|morningdayeveningnightfixed|morning-day-evening-night_fixed)
+    make_manifest "${ROOT}"/morning-day-evening-night_fixed.json
+    ;;
+  *)
+    echo "ERROR: ${TYPE@Q} not understood. Exiting."
+    exit 3
+    ;;
+esac
---
+++ update_manifests.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+set -eu
+
+./templates/create_manifest.sh -d 24hours -t morning-day-evening-night_fixed
+./templates/create_manifest.sh -d akihabarasouthexit -t day-sunset-night
+# ./templates/create_manifest.sh -d catherineroom
+# ./templates/create_manifest.sh -d corporationhall
+./templates/create_manifest.sh -d corporationstreet -t day-sunset-night
+./templates/create_manifest.sh -d elliemansion -t day-sunset-night
+./templates/create_manifest.sh -d firewatch -t day-sunset-night
+./templates/create_manifest.sh -d himitsuhouse -t day-sunset-night
+# ./templates/create_manifest.sh -d himitsuhouseinterior
+./templates/create_manifest.sh -d island -t day-night
+./templates/create_manifest.sh -d japanesegarden -t day-sunset-night
+./templates/create_manifest.sh -d japanesevillagehouse -t morning-day-sunset-night
+./templates/create_manifest.sh -d japanesevillagehouseinterior -t morning-day-sunset-night
+./templates/create_manifest.sh -d jrstation -t day-sunset-night
+# ./templates/create_manifest.sh -d kagomeroom
+./templates/create_manifest.sh -d kyosukeroom -t day-sunset-night
+./templates/create_manifest.sh -d mechanicalcity -t day-night
+./templates/create_manifest.sh -d metropolis -t day-night
+./templates/create_manifest.sh -d michaelroom -t day-sunset-night
+./templates/create_manifest.sh -d mountainside -t day-night
+./templates/create_manifest.sh -d oldhouse -t day-sunset-night
+./templates/create_manifest.sh -d oldhouselivingdiningrooms -t day-sunset-night
+# ./templates/create_manifest.sh -d oldhouseroom
+# ./templates/create_manifest.sh -d restaurantinterior
+# ./templates/create_manifest.sh -d restaurantstreet
+./templates/create_manifest.sh -d riverside -t day-sunset-night
+# ./templates/create_manifest.sh -d ryokanroom
+./templates/create_manifest.sh -d smalljapaneseprovincialtown -t morning-day-sunset-night
+./templates/create_manifest.sh -d streetclub -t day-night
+./templates/create_manifest.sh -d tokyostreet -t day-sunset-night
+./templates/create_manifest.sh -d twoqueenscities -t day-night
+./templates/create_manifest.sh -d yunjingcity -t day-night
